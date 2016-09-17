e285ab6ec8f8f180281ede3791c3c1ed599d5e43
HONG ZHOU <holly_zhou@yahoo.com>
2016-09-17 00:03:20 -0700

    get transaction

--- main.js
+++ main.js
@@ -2,21 +2,24 @@
 let App = React.createClass({
   getInitialState() {
     return {
-      transactions: [],
-      type: ''
+      transactions: []
     }
   },
 
-  getType(type){
-    this.setState({ type });
+  addTransaction(transaction) {
+    const { transactions } = this.state;
+    this.setState({
+      transactions: [...transactions, transaction]
+    })
   },
 
   render() {
+    const { transactions } = this.state;
     return (
       <div className="container">
         <div className="row">
           <h4>
-            <TransactionForm getType={this.getType}/>
+            <TransactionForm addTransaction={this.addTransaction}/>
           </h4>
         </div>
         <div className="row">
@@ -33,15 +36,44 @@ let App = React.createClass({
 });
 
 const TransactionForm = React.createClass({
+  getInitialState() {
+    return {
+      type: '',
+      amount: 0,
+      description: ''
+    }
+  },
 
   selectType(event) {
-    
-  }
+    let type = event.target.value;
+    this.setState({ type });
+  },
+
+  amountChange(event) {
+    let amount = event.target.value;
+    this.setState({ amount });
+  },
+
+  descriptionChange(event) {
+    let description = event.target.value;
+    this.setState({ description });
+  },
+
+  submitForm(event) {
+    event.preventDefault();
+    let transaction = {
+      type: this.state.type,
+      amount: this.state.amount,
+      description: this.state.description,
+      createAt: Date.now()
+    };
+    this.props.addTransaction(transaction);
+  },
 
   render() {
     return (
       <div>
-        <form className="form-inline">
+        <form className="form-inline" >
           <label className="radio-inline">
             <input type="radio" name="type" id="inlineRadio1" value="credit" onClick={this.selectType}/> Credit
           </label>
@@ -53,12 +85,26 @@ const TransactionForm = React.createClass({
             <label className="sr-only">Amount (in dollars)</label>
             <div className="input-group">
               <div className="input-group-addon">$</div>
-              <input type="text" className="form-control" id="exampleInputAmount" placeholder="Amount" />
+              <input
+                type="number"
+                min="0"
+                className="form-control"
+                id="exampleInputAmount"
+                placeholder="Amount"
+                value={this.state.amount}
+                onChange={this.amountChange}
+              />
             </div>
           </div>
           <span>  For: </span>
-          <input type="text" className="form-control" placeholder="Description"/>
-          <button type="submit" className="btn btn-primary">Submit</button>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Description"
+            value={this.state.description}
+            onChange={this.descriptionChange}
+          />
+          <button className="btn btn-primary" onClick={this.submitForm}>Submit</button>
         </form>
       </div>
     )
